bc1d6398e2aedc36e27bb0f19bcd49f98fee3b3d
CyCle1024 <chenchiyu@pjlab.org.cn>
2022-07-06 15:46:51 +0800

    feat: add support for following operations: 1. empty 2. ones 3. zeros 4. zeros_like 5. zero_ 6. randn 7. randperm 8. fill_ 9. full 10. full_like_ 11. masked_fill 12. masked_fill_

---
+++ long_tail_bench/samples/basic/fill_/torch_impl.py
@@ -0,0 +1,42 @@
+import torch
+import numpy as np
+from long_tail_bench.core.executer import Executer
+
+
+dtype_mapping = {
+    "bool": torch.bool,
+    "float32": torch.float32, 
+    "float": torch.float, 
+    "float64": torch.float64, 
+    "double": torch.double, 
+    "float16": torch.float16, 
+    "bfloat16": torch.bfloat16, 
+    "half": torch.half, 
+    "uint8": torch.uint8, 
+    "int8": torch.int8, 
+    "int16": torch.int16, 
+    "short": torch.short, 
+    "int32": torch.int32, 
+    "int": torch.int, 
+    "int64": torch.int64, 
+    "long": torch.long
+}
+
+
+def fill_(input_image, value):
+    ret = input_image.fill_(value)
+    return ret
+
+def args_adaptor(np_args):
+    size = np_args[0]
+    value = np_args[1]
+    dtype_str = np_args[2]
+    dtype = None
+    if dtype_str != "None":
+        dtype = dtype_mapping[dtype_str]
+    input_image_np = np.random.random(size)
+    input_image = torch.from_numpy(input_image_np).to(dtype).cuda()
+    return [input_image, value]
+
+def executer_creator():
+    return Executer(fill_, args_adaptor)
