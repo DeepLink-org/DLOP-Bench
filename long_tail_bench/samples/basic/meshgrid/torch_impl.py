e831115be232cb94362df358ee96ce1c3e4fa350
JinMinxi <jinminxi@sensetime.com>
2022-07-08 16:56:26 +0800

    added forward of argmax-meshgrid-cumsum

---
+++ long_tail_bench/samples/basic/meshgrid/torch_impl.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn
+import numpy as np
+from long_tail_bench.core.executer import Executer
+
+
+def meshgrid(*inputs):
+    input_images = []
+    print(inputs)
+    for input_size in inputs:
+        input_image_np = np.random.random(input_size)
+        input_image = torch.from_numpy(input_image_np).to(torch.float32).cuda()
+        input_images.append(input_image)
+    return torch.meshgrid(input_images)
+
+
+def args_adaptor(np_args):
+    print(np_args)
+    return np_args
+
+
+def executer_creator():
+    return Executer(meshgrid, args_adaptor)
