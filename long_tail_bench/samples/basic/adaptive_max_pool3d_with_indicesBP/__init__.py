a25ac2c5edd16f417d674ee015e17e226f62dc87
dianhong <dianhong.xu@intel.com>
2022-07-15 08:17:07 +0800

    [samples] Add pooling-related cases.

---
+++ long_tail_bench/samples/basic/adaptive_max_pool3d_with_indicesBP/__init__.py
@@ -0,0 +1,33 @@
+from long_tail_bench.common import (
+    SampleConfig,
+    register_sample,
+    SampleSource,
+    SampleTag,
+)
+import numpy as np
+import json
+
+def get_sample_config():
+    with open("./long_tail_bench/samples/basic/adaptive_max_pool3d_with_indicesBP/adaptive_max_pool3d_with_indices.json", "r") as f:
+        arg_data = json.load(f)
+    arg_data_length = len(arg_data["input"])
+    args_cases_ = []
+    for i in range(arg_data_length):
+        args_cases_.append((arg_data["input"][i], arg_data["output_size"][i]))
+    return SampleConfig(
+        args_cases=args_cases_,
+        requires_grad=[False] * 2,
+        backward=[False],
+        performance_iters=1000,
+        save_timeline=False,
+        source=SampleSource.MMDET,
+        url="",  # noqa
+        tags=[SampleTag.ViewAttribute],
+    )
+
+def gen_np_args(input_, output_size_):
+    input_image_np = np.random.random(input_)
+    output_size = output_size_
+    return [input_image_np, output_size]
+
+register_sample(__name__, get_sample_config, gen_np_args)
---
+++ long_tail_bench/samples/basic/avg_pool2d/torch_impl.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn
+import numpy as np
+from long_tail_bench.core.executer import Executer
+
+def avg_pool2d(input_image, kernel_size_image,\
+        stride_image, padding_image, ceil_mode_image, count_include_pad_image,\
+        divisor_override_image):
+    ret = torch.nn.functional.avg_pool2d(input_image, kernel_size_image,\
+        stride_image, padding_image, ceil_mode_image, count_include_pad_image,\
+        divisor_override_image)
+    return ret
+
+def args_adaptor(np_args):
+    input_image = np_args[0]
+    kernel_size = np_args[1]
+    stride = np_args[2]
+    padding = np_args[3]
+    ceil_mode = np_args[4]
+    count_include_pad = np_args[5]
+    divisor_override =  np_args[6]
+    if (len(kernel_size)) >1:
+        kernel_size_image = tuple(kernel_size)
+    else:
+        kernel_size_image = kernel_size[0]
+    if (len(stride)) >1:
+        stride_image = tuple(stride)
+    else:
+        stride_image = stride[0]
+    if (len(padding)) >1:
+        padding_image = tuple(padding)
+    else:
+        padding_image = padding[0]
+    ceil_mode_image = ceil_mode[0]
+    count_include_pad_image = count_include_pad[0]
+    divisor_override_image = divisor_override[0]
+    return [input_image, kernel_size_image,\
+        stride_image, padding_image, ceil_mode_image, count_include_pad_image,\
+        divisor_override_image]
+
+def executer_creator():
+    return Executer(avg_pool2d, args_adaptor)
