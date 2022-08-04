ec39498efce4fdaff1e6fa9e72a5345f2462f6b4
JimyMa <hit16s105116@gmail.com>
2022-08-04 11:11:38 +0800

    2nd op list

---
+++ long_tail_bench/samples/basic/equal/__init__.py
@@ -0,0 +1,40 @@
+from long_tail_bench.common import (
+    SampleConfig,
+    register_sample,
+    SampleSource,
+    SampleTag,
+)
+import numpy as np
+import json
+
+
+def get_sample_config():
+    with open("./long_tail_bench/samples/basic/equal/equal.json", "r") as f:
+        arg_data = json.load(f)
+    arg_data_length = len(arg_data["equal_0"])
+    args_cases_ = []
+    for i in range(arg_data_length):
+        args_cases_.append((arg_data["equal_0"][i], arg_data["equal_1"][i]))
+    return SampleConfig(
+        args_cases=args_cases_,
+        requires_grad=[False] * 2,
+        backward=[False],
+        performance_iters=1000,
+        save_timeline=False,
+        source=SampleSource.MMDET,
+        url="",  # noqa
+        tags=[SampleTag.ViewAttribute],
+    )
+
+
+def gen_np_args(equal_0, equal_1):
+    equal_0 = np.random.random(equal_0)
+    if isinstance(equal_1, list): # equal_1 is a tensor
+        equal_1 = np.random.random(equal_1)
+    else: # equal_1 is an imm
+        equal_1 = np.array(equal_1)
+
+    return [equal_0, equal_1]
+
+
+register_sample(__name__, get_sample_config, gen_np_args)
---
+++ long_tail_bench/samples/basic/round_/torch_impl.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn
+from long_tail_bench.core.executer import Executer
+
+def round_(round__0):
+    return round__0.round_()
+
+def args_adaptor(np_args):
+    round__0 = torch.from_numpy(np_args[0]).cuda()
+    return [round__0]
+
+
+def executer_creator():
+    return Executer(round_, args_adaptor)
