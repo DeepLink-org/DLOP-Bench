e2ef801bbcbee2b75fb7588431321e10f70b80fa
jinminxi <jinminxi@pjlab.org.cn>
2022-08-18 10:25:38 +0800

    add max op

---
+++ long_tail_bench/samples/basic/max/flops_cnt.py
@@ -0,0 +1,82 @@
+import json
+import matplotlib.pyplot as plt
+
+with open("./max.all.json", 'r') as f:
+    data = json.load(f)
+calc_cnt = []
+max_ = 0
+maxs1 = []
+mins1 = []
+maxs2 = []
+mins2 = []
+
+maxd = []
+mind = []
+min_ = 9999999
+for i in range(len(data['input'])):
+    cnt = 1
+    for d in range(len(data['input'][i])):
+        cnt = cnt * data['input'][i][d]
+    if cnt > max_:
+        maxs1 = data['input'][i]
+        max_ = cnt
+    if cnt < min_:
+        mins1 = data['input'][i]
+        min_ = cnt
+    calc_cnt.append(cnt)
+# calc_sum = sum(calc_cnt)
+# print(calc_sum)
+# calc_cnt = [i/calc_sum for i in calc_cnt]
+# print(calc_cnt)
+# gn = int((max(calc_cnt) - min(calc_cnt))/10000)
+print("max:")
+print(max(calc_cnt))
+print(maxs1)
+print("min:")
+print(min(calc_cnt))
+print(mins1)
+
+# plt.hist(calc_cnt, bins=20, edgecolor='k', density=1)
+plt.hist(calc_cnt, bins=20, edgecolor='k', density=False)
+plt.xlabel('Output shape size')
+plt.ylabel('Count')
+# plt.text()
+# plt.show()
+plt.savefig('max.flops_cnt.jpg')
+
+'''
+innermost = 0
+non_innermost = 0
+reduce_ = []
+maxr = 0
+minr = 999999999
+maxrs = []
+minrs = []
+for i in range(len(data['input_shape'])):
+    if data['dim'][i] == -1 or data['dim'][i] == (len(data['input_shape'][i]) - 1):
+        innermost += 1
+    else:
+        non_innermost += 1
+    reduce_.append(data['input_shape'][i][data['dim'][i]])
+    if maxr < data['input_shape'][i][data['dim'][i]]:
+        maxrs = data['input_shape'][i]
+        #maxrd = data['dim'][i]
+        maxr = data['input_shape'][i][data['dim'][i]]
+        maxd = data['dim'][i]
+    print(data['input_shape'][i][data['dim'][i]])
+    if data['input_shape'][i][data['dim'][i]] < minr:
+        minrs = data['input_shape'][i]
+        minr = data['input_shape'][i][data['dim'][i]]
+        mind = data['dim'][i]
+       
+
+print(maxrs)
+print(maxd)
+print(minrs)
+print(mind)
+plt.hist(reduce_, bins = 10)
+plt.show()
+#print(innermost)
+#print(non_innermost)
+
+'''
--- long_tail_bench/samples/basic/max/torch_impl.py
+++ long_tail_bench/samples/basic/max/torch_impl.py
@@ -2,15 +2,14 @@ import torch
 import torch.nn
 from long_tail_bench.core.executer import Executer
 
-def max(max1, max2):
-    if len(max2) == 0:
+def max(max1, dim):
+    if dim[0] == -999:
         return torch.max(max1)
-    return torch.max(max1, max2)
+    return torch.max(max1, dim[0])
 
 def args_adaptor(np_args):
     max1 = torch.from_numpy(np_args[0]).to(torch.float32).cuda()
-    max2 = torch.from_numpy(np_args[1]).to(torch.float32).cuda()
-    return [max1, max2]
+    return [max1, np_args[1]]
 
 
 def executer_creator():
