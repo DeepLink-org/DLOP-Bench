6a7b745b26076fb2f7f88e2d2365f35c9dd3f4e5
yaofengchen <fengchengyao@foxmail.com>
2022-08-19 13:05:25 +0800

    add normalize json files and modify run.py

---
+++ long_tail_bench/samples/basic/normalize/__init__.py
@@ -0,0 +1,37 @@
+from long_tail_bench.common import (
+    SampleConfig,
+    register_sample,
+    SampleSource,
+    SampleTag,
+)
+import numpy as np
+import json
+
+
+def get_sample_config():
+    with open("./long_tail_bench/samples/basic/normalize/normalize.json", "r") as f:
+        arg_data = json.load(f)
+    arg_data_length = len(arg_data["input"])
+    args_cases_ = []
+    for i in range(arg_data_length):
+        args_cases_.append((arg_data["input"][i], arg_data["p"][i], 
+            arg_data["dim"][i], arg_data["eps"][i], arg_data["out"][i]))
+    return SampleConfig(
+        args_cases=args_cases_,
+        requires_grad=[False] * 5,
+        backward=[False],
+        performance_iters=1000,
+        save_timeline=False,
+        source=SampleSource.UNKNOWN,
+        url="",  # noqa
+        tags=[],
+    )
+
+
+def gen_np_args(input_, p_, dim_, eps_, out_):
+    input = np.random.random(input_).astype(np.float32)
+
+    return [input, p_, dim_, eps_, out_]
+
+
+register_sample(__name__, get_sample_config, gen_np_args)
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 
-op_list = ["linear", "bmm", "ger", "mv", "rand", "randperm", "norm", "einsum", "topk", "sort"]
+op_list = ["linear", "bmm", "ger", "mv", "rand", "randperm", "norm", "einsum", "normalize", "topk", "sort"]
 
 for op_name in op_list:
     run_command = "srun -p caif_debug --gres=gpu:1 bash run.sh {}".format(op_name)
