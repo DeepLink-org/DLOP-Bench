2d82a8913059f4115707907bb68dca82acb329fb
yaofengchen <fengchengyao@foxmail.com>
2022-08-22 14:23:42 +0800

    update run.py and README.md

--- run.py
+++ run.py
@@ -1,13 +1,23 @@
 import os
 
 op_list = ["linear", "bmm", "ger", "mv", "rand", "randperm", "norm", "einsum", "normalize", "topk", "sort"]
+gpu_name = "V100"
 
 for op_name in op_list:
+    # test perf
     run_command = "srun -p caif_debug --gres=gpu:1 bash run.sh {}".format(op_name)
     os.system(run_command)
-    gpu_name = "V100"
+    
+    # rename perf csv
     dir_name = "perf_result"
     old_name = op_name + "_perf.csv"
     new_name = op_name + "_" + gpu_name + "_perf.csv"
+    rename_command = "cd {} && mv {} {} && cd ..".format(dir_name, old_name, new_name)
+    os.system(rename_command)
+
+    # rename profiler txt
+    dir_name = "profiler_result"
+    old_name = op_name + "_profiler.txt"
+    new_name = op_name + "_" + gpu_name + "_profiler.txt"
     rename_command = "cd {} && mv {} {} && cd ..".format(dir_name, old_name, new_name)
     os.system(rename_command)
