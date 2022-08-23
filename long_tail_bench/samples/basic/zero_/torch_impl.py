c7a34a61cef0789cbda6f7cb8f5952195d435116
liuhangda <liuhangda@pjlab.org.cn>
2022-08-23 16:50:19 +0800

    fix bug

---
+++ long_tail_bench/samples/basic/zero_/torch_impl.py
@@ -0,0 +1,16 @@
+import torch
+import numpy as np
+from long_tail_bench.core.executer import Executer
+
+def zero_(input_image):
+    ret = torch.zero_(input_image)
+    return ret
+
+def args_adaptor(np_args):
+    size = np_args[0]
+    input_image_np = np.random.random(size)
+    input_image = torch.from_numpy(input_image_np).to(torch.float32).cuda()
+    return [input_image]
+
+def executer_creator():
+    return Executer(zero_, args_adaptor)
