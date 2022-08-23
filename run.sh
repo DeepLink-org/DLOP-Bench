413dd667584153651c89b7cc9b738ca07af2a6f3
zhoushenglong <zhoushenglong@pjlab.org.cn>
2022-08-23 14:34:59 +0800

    remove some extra files.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=./long_tail_bench:$PYTHONPATH
-# FRAMEWORK=torch SAMPLE_IMPL=torch python ./long_tail_bench/api/api.py -st 1 -c $1 index_select
 # FRAMEWORK=torch SAMPLE_IMPL=torch python ./long_tail_bench/api/api.py -st 1 -c $1 masked_fill
+# FRAMEWORK=torch SAMPLE_IMPL=torch python ./long_tail_bench/api/api.py -st 1 -c $1 index_select
 # FRAMEWORK=torch SAMPLE_IMPL=torch python ./long_tail_bench/api/api.py -st 1 -c $1 masked_fill_
 FRAMEWORK=torch SAMPLE_IMPL=torch python ./long_tail_bench/api/api.py -st 1 -c $1 cat
 # FRAMEWORK=torch SAMPLE_IMPL=torch python ./long_tail_bench/api/api.py -st 1 -c $1 stack
