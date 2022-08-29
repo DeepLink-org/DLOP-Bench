089944eba4b58ded70e343ea7a4dc9762dff1c60
liuhangda <liuhangda@pjlab.org.cn>
2022-08-29 17:19:05 +0800

    topk bug fix

--- bench/samples/basic/topk/torch_impl.py
+++ bench/samples/basic/topk/torch_impl.py
@@ -9,8 +9,7 @@ def topk(topk_0, topk_1):
 
 def args_adaptor(np_args):
     topk_0 = torch.from_numpy(np_args[0]).cuda()
-    topk_1 = torch.from_numpy(np_args[1]).cuda()
-    return [topk_0, topk_1]
+    return [topk_0, np_args[1]]
 
 
 def executer_creator():
