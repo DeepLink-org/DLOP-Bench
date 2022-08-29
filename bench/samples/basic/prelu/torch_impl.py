dc46ec49e30afc01fd0c2f50e0b709af261dfba5
liuhangda <liuhangda@pjlab.org.cn>
2022-08-29 16:41:32 +0800

    bugfix

--- bench/samples/basic/prelu/torch_impl.py
+++ bench/samples/basic/prelu/torch_impl.py
@@ -8,8 +8,8 @@ def prelu(prelu_0, prelu_1):
     return torch.prelu(prelu_0, prelu_1)
 
 def args_adaptor(np_args):
-    prelu_0 = torch.from_numpy(np_args[0]).cuda()
-    prelu_1 = torch.from_numpy(np_args[1]).cuda()
+    prelu_0 = torch.from_numpy(np_args[0]).to(torch.float32).cuda()
+    prelu_1 = torch.from_numpy(np_args[1]).to(torch.float32).cuda()
     return [prelu_0, prelu_1]
 
 
